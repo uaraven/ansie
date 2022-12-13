5391d5952694cc221137ac4489f7cb0fd866cbd9
Ole <ovoronin@gmail.com>
2022-12-12 20:03:07 -0500

    Add new API to convert RGB colours to 256-palette colours

--- ansie.go
+++ ansie.go
@@ -414,13 +414,13 @@ func (ap *AnsiBuffer) BgRgbI(i uint) *AnsiBuffer {
 // Values beyond range of [0..5] are clipped
 // R, G an B values are combined to get one of 216 colours supported by terminal
 func (ap *AnsiBuffer) FgRgb6(r, g, b uint) *AnsiBuffer {
-	colour := ap.sixColourCube(r, g, b)
+	colour := ap.RgbTo216Colours(r, g, b)
 	return ap.Fg(colour)
 }
 
 // BgRgb6 sets background RGB colour converted to 9-bit colour (3;3;3) as supported by 256-colour ANSI sequence
 func (ap *AnsiBuffer) BgRgb6(r, g, b uint) *AnsiBuffer {
-	colour := ap.sixColourCube(r, g, b)
+	colour := ap.RgbTo216Colours(r, g, b)
 	return ap.Bg(colour)
 }
 
@@ -486,6 +486,13 @@ func (ap *AnsiBuffer) EscM(codes ...int) *AnsiBuffer {
 	return ap
 }
 
+// RgbTo216Colours converts a colour represented as R,G,B values of 0 to 5 to one of 216 colours
+// in 256-colour palette
+func (ap *AnsiBuffer) RgbTo216Colours(r uint, g uint, b uint) Colour {
+	colour := 16 + 36*clip(r, 5) + 6*clip(g, 5) + clip(b, 5)
+	return Colour(colour)
+}
+
 func (ap *AnsiBuffer) writeAnsiCommand(command rune, sep rune, codes ...int) {
 	if ap.enabled {
 		ap.content.WriteString(esc)
@@ -499,11 +506,6 @@ func (ap *AnsiBuffer) writeAnsiCommand(command rune, sep rune, codes ...int) {
 
 func (ap *AnsiBuffer) writeAnsiSeq(codes ...int) {
 	ap.writeAnsiCommand('m', ';', codes...)
-}
-
-func (ap *AnsiBuffer) sixColourCube(r uint, g uint, b uint) Colour {
-	colour := 16 + 36*clip(r, 5) + 6*clip(g, 5) + clip(b, 5)
-	return Colour(colour)
 }
 
 func clip(c uint, high uint) uint {
